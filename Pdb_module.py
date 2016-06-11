4227f9635a768b55bb72cafe8fe499d88d395496
piekarz007 <jakubnaj@gmail.com>
2016-06-11 13:24:34 +0200

    Update Pdb_module.py

--- Pdb_module.py
+++ Pdb_module.py
@@ -1,5 +1,7 @@
 import urllib.request
 import xmltodict
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree
 from json import loads, dumps
 from collections import OrderedDict, Counter
 from itertools import repeat, chain
@@ -11,118 +13,94 @@ from json import loads, dumps
 import warnings
 
 
-def informacja(pdb_id, url_root='http://www.rcsb.org/pdb/rest/describeMol?structureId='):
-    '''
-        Input:
-        pdb_id : string
-        A 4 character string giving a pdb entry
-        Output:
-        OrderedDict:
-        structureId,
-        length,
-        type etc
-    '''
-    url = url_root + pdb_id
-    req = urllib.request.Request(url)
-    temp = urllib.request.urlopen(req)
+def information(id, path_url='http://www.rcsb.org/pdb/rest/describeMol?structureId='):
+    url = path_url + id
+    request = urllib.request.Request(url)
+    temp = urllib.request.urlopen(request)
     result = temp.read()
     assert result
     out = xmltodict.parse(result, process_namespaces=True)
-    print(json.dumps(out, indent=4))
     return out
 
 
-def get_pdb_file(pdb_id, filetype='pdb', compression=False):
-    '''
-    Input:
-    pdb_id - string a 4 character giving a pdb entry
-    filetype - string
-    capabilities:
-    - pdb
-    - cif
-    - xml
-    compression - bool retrieve compressed (gz) version of the file
-    '''
-    if (len(pdb_id) == 4 and (compression == True or compression == False) and
+def show_information(id):
+    out = information(id, path_url='http://www.rcsb.org/pdb/rest/describeMol?structureId=')
+    out = json.dumps(out, indent=4)
+    return out
+
+
+def file_pdb(id, filetype='pdb', compression=False):
+    if (len(id) == 4 and (compression == True or compression == False) and
             (filetype == 'pdb' or filetype == 'cif' or filetype == 'xml')):
-        url = 'http://www.rcsb.org/pdb/download/downloadFile.do?fileFormat='
-        url += filetype
+        path_url = 'http://www.rcsb.org/pdb/download/downloadFile.do?fileFormat='
+        path_url += filetype
         if compression:
-            url += '&compression=YES'
+            path_url += '&compression=YES'
         else:
-            url += '&compression=NO'
-        url += '&structureId=' + pdb_id
-        req = urllib.request.Request(url)
-        f = urllib.request.urlopen(req)
-        result = f.read()
+            path_url += '&compression=NO'
+            path_url += '&structureId=' + id
+        request = urllib.request.Request(path_url)
+        temp = urllib.request.urlopen(request)
+        result = temp.read()
         result = result.decode('unicode_escape')
-        print(result[:1000])
+        file = open(id + '-pdb_file.txt', 'w')
+        file.write(result)
+        file.close
         return result
     else:
         print('invalid input')
 
 
-def describe_pdb(pdb_id):
-    '''
-    Description and metadata of a PDB  entry
-    Input:
-    pdb_id: string - 4 character
-    Out: description from PDB
-    '''
-    if (len(pdb_id) == 4):
-        out = get_info(pdb_id, url_root='http://www.rcsb.org/pdb/rest/describePDB?structureId=')
-        out = remove_at_sign(out['PDBdescription']['PDB'])
-        print(json.dumps(out, indent=4))
+def pdb_describtion_metadata(id):
+    if (len(id) == 4):
+        out = information(id, path_url='http://www.rcsb.org/pdb/rest/describePDB?structureId=')
+        out = remove_sign(out['PDBdescription']['PDB'])
+        out = json.dumps(out, indent=4)
         return out
     else:
         print('Invalid input')
 
 
-def remove_at_sign(temp):
-    '''
-    Remove the "@" character from the beginning of key names in a dict()
-    '''
-    tagged_keys = [thing for thing in temp.keys() if thing.startswith('@')]
-    for tag_key in tagged_keys:
+def remove_sign(temp):
+    keys = [thing for thing in temp.keys() if thing.startswith('@')]
+    for tag_key in keys:
         temp[tag_key[1:]] = temp.pop(tag_key)
-
     return temp
 
 
-def get_raw_blast(pdb_id, output_form='HTML', chain_id='A'):
-    '''
-    Download full blast structure
-    Input: pdb_id , chain_id, output format
-    Output: entire blast
-    '''
-    url_root = 'http://www.rcsb.org/pdb/rest/getBlastPDB2?structureId='
-    url = url_root + pdb_id + '&chainId=' + chain_id + '&outputFormat=' + output_form
-    req = urllib.request.Request(url)
-    f = urllib.request.urlopen(req)
+def raw_blast_file(id, chain='A'):
+    url_path = 'http://www.rcsb.org/pdb/rest/getBlastPDB2?structureId='
+    url = url_path + id + '&chainId=' + chain + '&outputFormat=' + 'HTML'
+    request = urllib.request.Request(url)
+    f = urllib.request.urlopen(request)
     result = f.read()
     result = result.decode('unicode_escape')
+    soup = BeautifulSoup(result, "html.parser")
+    out = soup.get_text()
     assert result
-    print(result)
-    return result
-
-
-def describe_chemical(chem_id):
-    out = get_info(chem_id, url_root='http://www.rcsb.org/pdb/rest/describeHet?chemicalID=')
-    print(json.dumps(out, indent=4))
+    file = open(id + '-blast_file.txt', 'w')
+    file.write(out)
+    file.close
     return out
 
 
-def get_ligands(pdb_id):
-    if (len(pdb_id) > 3):
-        out = get_info(pdb_id, url_root='http://www.rcsb.org/pdb/rest/ligandInfo?structureId=')
-        print(json.dumps(out, indent=4))
+def chemical_description(id):
+    out = information(id, path_url='http://www.rcsb.org/pdb/rest/describeHet?chemicalID=')
+    out = json.dumps(out, indent=4)
+    return out
+
+
+def ligands_description(id):
+    if (len(id) > 3):
+        out = information(id, path_url='http://www.rcsb.org/pdb/rest/ligandInfo?structureId=')
+        out = json.dumps(out, indent=4)
         return out
     else:
         print('Invalid input')
 
-# get_ligands('1c2e')
-# describe_chemical('NAG')
-# describe_pdb('4zla')
-# get_pdb_file('4zpo', filetype='pdb', compression=False)
-# informacja("2j4W")
-# get_raw_blast('2ako', output_form='HTML', chain_id='A')
+# print(ligands_description('1cbe'))
+# print(chemical_description('a3c'))
+# print(pdb_describtion_metadata('4cla'))
+# file_pdb('4zpo', filetype='pdb', compression=False)
+# print(show_information("2CA4"))
+# raw_blast_file('2csko', chain='A')
