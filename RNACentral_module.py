f0dfe0c8662c0f2b4c38a867889bfe2eb3f62a41
piekarz007 <jakubnaj@gmail.com>
2016-06-11 13:25:00 +0200

    Update RNACentral_module.py

--- RNACentral_module.py
+++ RNACentral_module.py
@@ -2,33 +2,23 @@ import hashlib
 import requests
 
 
-def get_md5(sequence):
-    """
-    Calculate md5 for an RNA sequence
-    """
-    # RNAcentral stores DNA md5 hashes
-    sequence = sequence.replace('U', 'T')
-    # get the md5 digest
+def calculate_md5(seq):
+    seq = seq.replace('U', 'T')
     m = hashlib.md5()
-    m.update(sequence.encode("utf8"))
+    m.update(seq.encode("unicode_escape"))
     return m.hexdigest()
 
 
-# get the RNAcentral id
-def get_rnacentral_id(md5):
-    """
-    Parse json output and return the RNAcentral id.
-    """
+def rnacentral_id(md5):
     url = 'http://rnacentral.org/api/v1/rna'
-    r = requests.get(url, params={'md5': md5})
-    data = r.json()
+    temp = requests.get(url, params={'md5': md5})
+    data = temp.json()
     if data['count'] > 0:
         return data['results'][0]['rnacentral_id']
     else:
-        return 'RNAcentral id not found'
+        return 'iD not found'
 
 
-# This sequence has an RNAcentral id
 sequence = 'CUGAAUAAAUAAGGUAUCUUAUAUCUUUUAAUUAACAGUUAAACGCUUCCAUAAAGCUUUUAUCCA'
-md5 = get_md5(sequence)
-print(get_rnacentral_id(md5))  # URS00002C9E9D
+md5 = calculate_md5(sequence)
+print(rnacentral_id(md5))
