eed94df3cc42ae060e0b51f1f82f23e04577178f
piekarz007 <jakubnaj@gmail.com>
2016-06-11 15:05:47 +0200

    Update RNACentral_module.py

--- RNACentral_module.py
+++ RNACentral_module.py
@@ -1,5 +1,7 @@
 import hashlib
 import requests
+import urllib.request
+import json
 
 
 def calculate_md5(seq):
@@ -19,6 +21,62 @@ def rnacentral_id(md5):
         return 'iD not found'
 
 
+def information_RNACentral(id):
+    path_url = 'http://rnacentral.org/api/v1/rna/'
+    url = path_url + id
+    try:
+        request = urllib.request.Request(url)
+        temp = urllib.request.urlopen(request)
+        result = temp.read()
+        result = result.decode('unicode_escape')
+        assert result
+        result = json.loads(result)
+        result = json.dumps(result, indent=4)
+        return result
+    except:
+        print('Invalid Input')
+
+
+def publications_RNACentral(id):
+    path_url = 'http://rnacentral.org/api/v1/rna/' + id + '/publications'
+    print(path_url)
+    url = path_url
+    try:
+        request = urllib.request.Request(url)
+        temp = urllib.request.urlopen(request)
+        result = temp.read()
+        result = result.decode('unicode_escape')
+        assert result
+        result = json.loads(result)
+        result = json.dumps(result, indent=4)
+        return result
+    except:
+        print('Invalid Input')
+
+
+def xrefs_RNACentral(id):
+    path_url = 'http://rnacentral.org/api/v1/rna/' + id + '/xrefs'
+    print(path_url)
+    url = path_url
+    try:
+        request = urllib.request.Request(url)
+        temp = urllib.request.urlopen(request)
+        result = temp.read()
+        result = result.decode('unicode_escape')
+        assert result
+        result = json.loads(result)
+        result = json.dumps(result, indent=4)
+        file = open(id + '-xrefs_RNACentral.txt', 'w')
+        file.write(result)
+        file.close
+        return result
+    except:
+        print('Invalid Input')
+
+
 sequence = 'CUGAAUAAAUAAGGUAUCUUAUAUCUUUUAAUUAACAGUUAAACGCUUCCAUAAAGCUUUUAUCCA'
 md5 = calculate_md5(sequence)
-print(rnacentral_id(md5))
+# print(rnacentral_id(md5))
+# print(information_RNACentral('URS0000000001'))
+# print(publications_RNACentral('URS0000000001'))
+print(xrefs_RNACentral('URS0000000001'))
