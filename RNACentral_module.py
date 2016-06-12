a7863e4d064c5bbf54b4cf14819abb43013ac5db
piekarz007 <jakubnaj@gmail.com>
2016-06-12 16:43:31 +0200

    Update RNACentral_module.py

--- RNACentral_module.py
+++ RNACentral_module.py
@@ -7,13 +7,22 @@ from tkinter import filedialog
 
 
 def calculate_md5(seq):
+    """
+    Calculate md5 for an RNA sequence
+    """
+    # RNAcentral stores DNA md5 hashes
     seq = seq.replace('U', 'T')
+    # get the md5 digest
     m = hashlib.md5()
     m.update(seq.encode("unicode_escape"))
     return m.hexdigest()
 
 
+# get the RNAcentral id
 def rnacentral_id(md5):
+    """
+    Parse json output and return the RNAcentral id.
+    """
     url = 'http://rnacentral.org/api/v1/rna'
     temp = requests.get(url, params={'md5': md5})
     data = temp.json()
@@ -24,6 +33,17 @@ def rnacentral_id(md5):
 
 
 def information_RNACentral(id):
+    """"
+        This call provides a summary of properties of a RNACentral entry,
+        such as the title of the entry, number of entities,
+        date of deposition, revision date, release date,
+        experimental method, list of related entries in case split entries, etc.
+
+        :param id:  String - 4-character RNACentral id code.
+        :return:release_date, experimental_method_class, name, assembly_id,preferred,form,
+        deposition_site,related_structures,title,split_entry,number_of_entities,processing_site,
+        entry_authors,revision_date,deposition_date
+    """
     path_url = 'http://www.ebi.ac.uk/pdbe/api/pdb/entry/summary/'
     url = path_url + format(id)
     try:
@@ -47,6 +67,15 @@ def information_RNACentral(id):
 
 
 def publications_RNACentral(id):
+    """
+        This call provides details of publications associated with an entry,
+        such as title of the article, journal name,
+        year of publication, count, doi, pubmed_id, etc.
+        Primary citation is listed first.
+
+        :param id: String - 4-character PDB id code.
+        :return: next, previous, count, pubmed_id, publication, authors, pub_id, title, doi, pubmed_id
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/' + format(id) + '/publications'
     url = path_url
     try:
@@ -70,6 +99,15 @@ def publications_RNACentral(id):
 
 
 def xrefs_RNACentral(id):
+    """
+    List of cross-references for a particular RNA sequence such as
+    database, if is active,  first seen last seen etc
+
+    :param id: String - 4-character PDB id code.
+    :return: first_seen,last_seen,database,is_expert_db,taxid,is_active,
+    accession: product, source_url, description, species, rna_type, id, expert_db_url,
+    optional_id, url, citations, external_id, organelle, gene
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/' + format(id) + '/xrefs'
     print(path_url)
     url = path_url
@@ -94,6 +132,11 @@ def xrefs_RNACentral(id):
 
 
 def filter_length(len):
+    """
+    This call returns list of structures which have length equal len param.
+    :param len: Number - structure length
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?length='
     url = path_url + format(len)
     try:
@@ -120,6 +163,11 @@ def filter_length(len):
 
 
 def filter_min_length(len):
+    """
+    This call returns list of structures which have length equal or bigger than len param.
+    :param len: Number - specifies the minimum length
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?min_length='
     url = path_url + format(len)
     try:
@@ -146,6 +194,11 @@ def filter_min_length(len):
 
 
 def filter_max_length(len):
+    """
+    This call returns list of structures which have length equal or smaller than len param.
+    :param len: Number - specifies the maximum length
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?max_length='
     url = path_url + format(len)
     try:
@@ -172,6 +225,12 @@ def filter_max_length(len):
 
 
 def filter_min_max_length(min, max):
+    """
+    This call returns list of structures which have length between min and max params.
+    :param min: Number - specifies the minimum length
+    :param max: Number - specifies the maximum length
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?min_length=' + format(min) + '&max_length=' + format(max)
     try:
         request = urllib.request.Request(path_url)
@@ -197,6 +256,11 @@ def filter_min_max_length(min, max):
 
 
 def filter_by_database(database):
+    """
+    This call returns list of structures which are in database param.
+    :param database: String - Name of database
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?database='
     url = path_url + format(database)
     try:
@@ -223,6 +287,11 @@ def filter_by_database(database):
 
 
 def filter_by_external_id(id):
+    """
+    This call provides details about structure from external_id.
+    :param id: String - external id
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?external_id='
     url = path_url + format(id)
     try:
@@ -249,6 +318,12 @@ def filter_by_external_id(id):
 
 
 def filter_database_min_length(database, min):
+    """
+    This call returns list of structures which are in database param and have length bigger than min.
+    :param database: String - Name of database
+    :param min: Number - specifies the minimum length
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?database=' + format(database) + '&min_length=' + format(min)
     try:
         request = urllib.request.Request(path_url)
@@ -274,6 +349,12 @@ def filter_database_min_length(database, min):
 
 
 def filter_database_max_length(database, max):
+    """
+    This call returns list of structures which are in database param and have length smaller than max.
+    :param database: String - Name of database
+    :param max: Number - specifies the maximum length
+    :return: count, publications, md5, rnacentral_id, length, xrefs, sequence, url, next, previous
+    """
     path_url = 'http://rnacentral.org/api/v1/rna/?database=' + format(database) + '&max_length=' + format(max)
     try:
         request = urllib.request.Request(path_url)
@@ -297,9 +378,10 @@ def filter_database_max_length(database, max):
         else:
             print('Invalid Input')
 
+# This sequence has an RNAcentral id
+# sequence = 'CUGAAUAAAUAAGGUAUCUUAUAUCUUUUAAUUAACAGUUAAACGCUUCCAUAAAGCUUUUAUCCA'
+# md5 = calculate_md5(sequence)
 
-sequence = 'CUGAAUAAAUAAGGUAUCUUAUAUCUUUUAAUUAACAGUUAAACGCUUCCAUAAAGCUUUUAUCCA'
-md5 = calculate_md5(sequence)
 # print(rnacentral_id(md5))
 # print(information_RNACentral('1cbs'))
 # print(publications_RNACentral('URS0000000001'))
@@ -307,7 +389,7 @@ md5 = calculate_md5(sequence)
 # print(filter_length(2014))
 # print(filter_min_length(2014))
 # print(filter_max_length(2014))
-# print(filter_min_max_length(100,200))
+# print(filter_min_max_length(1000,2002))
 # print(filter_by_database('srpdb'))
 # print(filter_by_external_id('MIMAT0000091'))
 # print(filter_database_min_length('srpdb',200))
